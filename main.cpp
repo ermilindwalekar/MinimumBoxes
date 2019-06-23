96fba6b6cf8dba78003d37a24f50da8de8c70cbf
Milind Walekar <ermilindwalekar@gmail.com>
2019-06-23 13:53:16 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <map>
+
+#define SMALL 2
+#define MEDIUM 3
+#define LARGE 5
+
+int boxOfSpecificSize(int item, int capacity){
+   int boxes = item / capacity;
+   if(item % capacity != 0)
+    boxes++;
+
+   return boxes;
+}
+
+void findMinimalBoxes(int item, map<char, int>& solution){
+
+    //Handle when no items are left to be placed
+    if(item == 0)
+        return;
+
+    //If just 1 item is left to place
+    if(item == 1){
+        if(solution.insert({'S', 1}).second == false){
+            map<char, int>::iterator it = solution.find('S');
+            ++it->second;
+        }
+        return;
+    }
+
+    if(item >= LARGE){
+        if(solution.insert({'L', item/LARGE}).second == false){
+            map<char, int>::iterator it = solution.find('L');
+            ++it->second;
+        }
+        //Recursively call the function with left over items to place
+        findMinimalBoxes(item % LARGE, solution);
+        return;
+    }
+
+    if(item >= MEDIUM){
+        if(solution.insert({'M', item/MEDIUM}).second == false){
+            map<char, int>::iterator it = solution.find('M');
+            ++it->second;
+        }
+        findMinimalBoxes(item % MEDIUM, solution);
+        return;
+    }
+
+    if(item >= SMALL){
+        if(solution.insert({'S', item/SMALL}).second == false){
+            map<char, int>::iterator it = solution.find('S');
+            ++it->second;
+        }
+        findMinimalBoxes(item % SMALL, solution);
+    }
+}
+
+int main()
+{
+    //Storing type of box and quantity required
+    map<char,int> solution;
+    map<char,int>::iterator it;
+
+    findMinimalBoxes(104, solution);
+
+    for(it = solution.begin(); it != solution.end(); it++){
+        cout<< it->first << " " << it->second<<endl;
+    }
+    return 0;
+}
